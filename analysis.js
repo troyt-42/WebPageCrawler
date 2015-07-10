f78c36ed5bededda5d0ba5b644af1604d829eda0
Tao_Mac <rnm2888@gmail.com>
2015-07-10 00:22:44 -0400

    improve speed by reduing loops

--- analysis.js
+++ analysis.js
@@ -32,21 +32,24 @@ filesPromise.then(function(data){
   return Q.all(promises);
 }).then(function(){
   var $ = cheerio.load(generalData);
-  // var jobTitles = $('.jobTitleContainer').text().split(/\s{4,}/);
-  // console.log(jobTitles);
   var result = fs.createWriteStream("result.txt");
   result.write("Job                                                                                         Company \n");
-  for(var i = 0; i < 2500; i ++){
-    var job = $('.jobTitleContainer').eq(i).text().replace(/\s/g, '').match(/([A-Z][a-z]+)|([A-Z]+)|([a-z]+)/g).join(" ");
-    var company = $('.companyContainer .fnt4').eq(i).text().match(/([A-Z][^ ]*)|([a-z][^ ]*)/g).join(" ");
-    console.log(i);
-    var space = '';
-    for(var p = 0; p < "                                                                                            ".length - job.length; p++){
-      space += " ";
-    }
-    console.log(space + "|");
-    result.write(job + space + company + "\n");
-  }
+
+  $('.jobTitleCol.fnt4').each(function(index, element){
+      var node = $(element);
+      var job = $(element).find('.fnt11').text();
+      var company = $(element).find('.fnt4').attr('title');
+      console.log(index);
+      console.log(job);
+      console.log(company);
+
+      var space = '';
+      for(var p = 0; p < "                                                                                            ".length - job.length; p++){
+        space += " ";
+      }
+      console.log(space + "|");
+      result.write(job + space + company + "\n");
+  });
   result.end(function(){
     console.log('Done; Please Check the Result in result.txt');
   });
